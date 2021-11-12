9c3ebdc78363681d732ed97dd88c5d26d78ce6fc
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 14:07:05 -0300

    Utilizando perfis de aplicação

--- vendas/src/main/java/br/dev/mhc/vendas/MinhaConfiguration.java
+++ vendas/src/main/java/br/dev/mhc/vendas/MinhaConfiguration.java
@@ -1,9 +1,19 @@
 package br.dev.mhc.vendas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class MinhaConfiguration {
+	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("Rodando aplicação em desenvolvimento");			
+		};
+	}
 	
 }
