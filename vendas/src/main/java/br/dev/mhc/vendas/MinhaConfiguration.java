2ed5ea701de37bfc2243a7e44e7b982ab08bc7b0
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 13:05:12 -0300

    Add spring configuration

---
+++ vendas/src/main/java/br/dev/mhc/vendas/MinhaConfiguration.java
@@ -0,0 +1,14 @@
+package br.dev.mhc.vendas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+	
+	@Bean(name = "applicationName")
+	public String applicationName() {
+		return "Sistema de Vendas";
+	}
+
+}
