d4680d8e3ddfca5eb133145240c638e53ede2d00
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 16:53:36 -0300

    Adicionando operações com o banco de dados

--- vendas/src/main/java/br/dev/mhc/vendas/VendasApplication.java
+++ vendas/src/main/java/br/dev/mhc/vendas/VendasApplication.java
@@ -1,10 +1,31 @@
 package br.dev.mhc.vendas;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.dev.mhc.vendas.domain.entity.Cliente;
+import br.dev.mhc.vendas.domain.repositorio.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
+
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+
+			clientes.salvar(new Cliente("Marcelo"));
+			clientes.salvar(new Cliente("Henrique"));
+			
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
