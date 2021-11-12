985fa14887722ce8d7f48927b566fd3716c44561
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 17:36:57 -0300

    Adicionando operações com o banco de dados

--- vendas/src/main/java/br/dev/mhc/vendas/VendasApplication.java
+++ vendas/src/main/java/br/dev/mhc/vendas/VendasApplication.java
@@ -17,12 +17,38 @@ public class VendasApplication {
 	@Bean
 	public CommandLineRunner init(@Autowired Clientes clientes) {
 		return args -> {
+			
+			System.out.println("Salvando clientes");
 
 			clientes.salvar(new Cliente("Marcelo"));
 			clientes.salvar(new Cliente("Henrique"));
 			
 			List<Cliente> todosClientes = clientes.obterTodos();
 			todosClientes.forEach(System.out::println);
+			
+			System.out.println("Atualizando clientes");
+
+			todosClientes.forEach(c -> {
+				c.setNome(c.getNome() + " atualizado.");
+				clientes.atualizar(c);
+			});
+			todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+			
+			System.out.println("Buscando clientes");
+			clientes.buscarPorNome("Marce").forEach(System.out::println);
+			
+			System.out.println("Deletando clientes");
+			clientes.obterTodos().forEach(c -> {
+				clientes.deletar(c.getId());
+			});
+			
+			todosClientes = clientes.obterTodos();
+			if(todosClientes.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado.");
+			} else {
+				todosClientes.forEach(System.out::println);
+			}
 
 		};
 	}
--- vendas/src/main/java/br/dev/mhc/vendas/domain/repositorio/Clientes.java
+++ vendas/src/main/java/br/dev/mhc/vendas/domain/repositorio/Clientes.java
@@ -13,25 +13,47 @@ import br.dev.mhc.vendas.domain.entity.Cliente;
 
 @Repository
 public class Clientes {
-	
+
 	private static String INSERT = "insert into cliente (nome) values (?) ";
 	private static String SELECT_ALL = "select * from cliente ";
-	
+	private static String UPDATE = "update cliente set nome = ? where id = ? ";
+	private static String DELETE = "delete from cliente where id = ? ";
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	public Cliente salvar(Cliente cliente) {
-		jdbcTemplate.update(INSERT, new Object[] {cliente.getNome()});
+		jdbcTemplate.update(INSERT, new Object[] { cliente.getNome() });
 		return cliente;
 	}
-	
+
+	public Cliente atualizar(Cliente cliente) {
+		jdbcTemplate.update(UPDATE, new Object[] { cliente.getNome(), cliente.getId() });
+		return cliente;
+	}
+
+	public void deletar(Integer id) {
+		jdbcTemplate.update(DELETE, new Object[] { id });
+	}
+
+	public List<Cliente> buscarPorNome(String nome) {
+		return jdbcTemplate.query(
+				SELECT_ALL.concat(" where nome like ?"),
+				new Object[] {"%" + nome + "%"},
+				obterClienteMapper());		
+	}
+
 	public List<Cliente> obterTodos() {
-		return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+		return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+	}
+
+	private RowMapper<Cliente> obterClienteMapper() {
+		return new RowMapper<Cliente>() {
 			@Override
 			public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
 				return new Cliente(resultSet.getInt("id"), resultSet.getString("nome"));
 			}
-		});
+		};
 	}
 
 }
