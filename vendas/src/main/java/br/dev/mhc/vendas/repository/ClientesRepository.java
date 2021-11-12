ec8cef690330868189f95a80a46418c376ad5316
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 13:33:23 -0300

    Aplicando configurações Spring

--- vendas/src/main/java/br/dev/mhc/vendas/repository/ClientesRepository.java
+++ vendas/src/main/java/br/dev/mhc/vendas/repository/ClientesRepository.java
@@ -1,7 +1,10 @@
 package br.dev.mhc.vendas.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.dev.mhc.vendas.model.Cliente;
 
+@Repository
 public class ClientesRepository {
 
 	public void persistir(Cliente cliente) {
