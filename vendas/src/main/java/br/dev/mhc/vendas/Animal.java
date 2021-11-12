73b008ddc28c480fbfd5f9f3f153ca473ab3fc66
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-12 14:37:32 -0300

    Utilizando annotations personalizadas

---
+++ vendas/src/main/java/br/dev/mhc/vendas/Animal.java
@@ -0,0 +1,7 @@
+package br.dev.mhc.vendas;
+
+public interface Animal {
+	
+	public void fazerBarulho();
+
+}
--- vendas/src/main/java/br/dev/mhc/vendas/MinhaConfiguration.java
+++ vendas/src/main/java/br/dev/mhc/vendas/MinhaConfiguration.java
@@ -2,17 +2,15 @@ package br.dev.mhc.vendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Development
 public class MinhaConfiguration {
-	
+
 	@Bean
 	public CommandLineRunner executar() {
 		return args -> {
-			System.out.println("Rodando aplicação em desenvolvimento");			
+			System.out.println("Rodando aplicação em desenvolvimento");
 		};
 	}
-	
+
 }
